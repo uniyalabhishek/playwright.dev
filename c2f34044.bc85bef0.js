ce96631c09614a6a7541124747164510c4590811
arjunattam <arjunattam@users.noreply.github.com>
2020-12-16 05:07:25 +0000

    Apply automatic changes

--- c2f34044.bc85bef0.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[596],{665:function(e){e.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"releases","name":"releases","count":1,"permalink":"/blog/tags/releases"}')}}]);
---
+++ cb6d121a.139841cd.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[635],{705:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Playwright 1.6","permalink":"/blog/hello-world"}]}')}}]);
