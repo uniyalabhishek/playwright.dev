458f89827e2b8a74b115387597c5eb72afbbc39c
pavelfeldman <pavelfeldman@users.noreply.github.com>
2021-01-08 01:32:33 +0000

    Apply automatic changes

--- python/5faff484.dceebfd3.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[37],{106:function(e){e.exports=JSON.parse('{"releases":{"allTagsPath":"/python/blog/tags","slug":"releases","name":"releases","count":1,"permalink":"/python/blog/tags/releases"}}')}}]);
