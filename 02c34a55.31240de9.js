a463f116517cf7786c4b284dbf02a1253c9dd4b4
pavelfeldman <pavelfeldman@users.noreply.github.com>
2021-01-15 03:59:21 +0000

    Apply automatic changes

--- 02c34a55.31240de9.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{76:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Playwright 1.6","permalink":"/blog/hello-world"}]}')}}]);
