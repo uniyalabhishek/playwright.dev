821fbd97dd84af25fc0a0563d75d263bcf65fca4
pavelfeldman <pavelfeldman@users.noreply.github.com>
2021-02-25 19:05:25 +0000

    Apply automatic changes

---
+++ java/ab51106a.8a819e90.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[51],{122:function(a){a.exports=JSON.parse('{"releases":{"allTagsPath":"/java/blog/tags","slug":"releases","name":"releases","count":1,"permalink":"/java/blog/tags/releases"}}')}}]);
